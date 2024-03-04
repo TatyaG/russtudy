2e570dfd1d1e68c8d0efb3198cf317d777b97eab
maxim1955 <nikitin.maxim.99@gmail.com>
2024-03-04 20:31:56 +0300

    add services RKI

--- src/components/v-service-tabs.vue
+++ src/components/v-service-tabs.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="service_item__wrap">
     <div class="service_item__label">
-  <span
-      v-for="item of serviceName"
-      @click="emit('changeServiceTabs',item)"
-      class="service_item"
-      :class="{active_tab: item.name === selected_service.name}"
-  >
-    {{ item.label }}
-  </span>
+        <span
+            v-for="item of serviceName"
+            @click="emit('changeServiceTabs',item)"
+            class="service_item"
+            :class="{active_tab: item.name === props.selected_service.name}"
+        >
+          {{ item.label }}
+        </span>
+      <div class="">
+
+      </div>
+
+
+      <!--      <div class="horizontal-scroll service_item_mob">
+              <ul class="scrollable-list service_item_mob">
+                <li v-for="(item,index ) of serviceName"
+                    :key="index"
+                    :class="{active_tab: item.name === props.selected_service.name}"
+                    @click="emit('changeServiceTabs',item)"
+                >
+                  {{ item.label }}
+                </li>
+              </ul>
+            </div>-->
     </div>
   </div>
 
 </template>
 <script setup>
+
+
 const props = defineProps({
   serviceName: {
     type: Array,
@@ -25,11 +43,39 @@ const props = defineProps({
   }
 })
 const emit = defineEmits(["changeServiceTabs"])
+/*Slick slider */
 
-
+const items = [
+  { id: 1, text: 'Item 1' },
+  { id: 2, text: 'Item 2' },
+  { id: 3, text: 'Item 3' },
+  { id: 4, text: 'Item 4' },
+  { id: 5, text: 'Item 5' },
+];
 </script>
 <style scoped>
-.active_tab{
+.active_tab {
   text-decoration: underline;
+}
+
+/* Стили для листающегося списка по горизонтали */
+.horizontal-scroll {
+  overflow-x: auto; /* Добавляет горизонтальную полосу прокрутки при необходимости */
+}
+
+.scrollable-list {
+  white-space: nowrap; /* Предотвращает перенос текста на новую строку */
+  padding: 10px; /* Добавляет отступы вокруг списка */
+}
+
+.scrollable-list li {
+  display: inline-block; /* Размещает элементы списка в ряд */
+  margin-right: 8px; /* Отступ между элементами списка */
+}
+
+.slider-item {
+  text-align: center;
+  padding: 20px;
+  background: #ddd;
 }
 </style>
--- src/views/v-service.vue
+++ src/views/v-service.vue
@@ -3,9 +3,14 @@
                      @closemodal="createMaterial=!createMaterial">
   </v-create-material>
   <div class="container " v-if="!createMaterial">
-    <Header />
+    <Header/>
     <h4 class="service_title">{{ selectedService.label }}</h4>
     <div class="flex serv">
+      <div class="service_item_mob">
+        <v-service-tabs :service-name="serviceName"
+                        :selected_service="selectedService"
+                        @changeServiceTabs="changeServiceName"/>
+      </div>
       <div class="service_wrap ">
         <p class="service_nav_title">Сервисы РКИ</p>
         <v-service-tabs
@@ -55,9 +60,11 @@ const serviceName = [
   {name: 'Material', label: 'Учебно-методические материалы',},
   {name: 'Game', label: 'Игра',},
   {name: 'Tests', label: 'Тесты'},
+  {name: 'Kalinka', label: 'Kalinka - Russian Fast & Easy'},
+  {name: 'Course', label: 'Курсы'},
   {name: 'Teacher', label: 'База учителей'},
   {name: 'Vacancy', label: 'Вакансии'},
-  {name: 'Course', label: 'Курсы'},
+
 ]
 const tests = ref([
   {id: 1, name: 'Уровень A1', image: '../img/tests/a1.svg'},
@@ -65,7 +72,6 @@ const tests = ref([
   {id: 3, name: 'Уровень B1', image: '../img/tests/b1.svg'},
   {id: 4, name: 'Уровень B2', image: '../img/tests/b2.svg'},
   {id: 5, name: 'Уровень C1', image: '../img/tests/c1.svg'},
-  {id: 6, name: 'Уровень C2', image: '../img/tests/c2.svg'},
 ])
 const selectedService = ref({name: 'Material', label: 'Учебно-методические материалы'})
 </script>
